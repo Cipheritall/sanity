6b9037a967becbfd03bb6002b58e6f5fd1ca0a7b
0xC137 <91955270+streetU@users.noreply.github.com>
2022-06-21 14:56:27 +0200

    slug game

--- schemas/game.js
+++ schemas/game.js
@@ -6,6 +6,15 @@ export default {
             name: "name",
             title: "Name",
             type: "string",
+        },
+        {
+          name: 'slug',
+          title: 'Slug',
+          type: 'slug',
+          options: {
+            source: 'title',
+            maxLength: 96
+          }
         }, {
             name: "thumbnail",
             title: "Thumbnail",

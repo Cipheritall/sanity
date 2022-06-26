540a62b29373228966ef4b9719b3bbe4c2f835ca
0xC137 <91955270+streetU@users.noreply.github.com>
2022-06-26 16:43:48 +0200

    AUTH

---
+++ schemas/auth.js
@@ -0,0 +1,23 @@
+export default {
+    name: "auth",
+    title: "External Authentification",
+    type: "document",
+    fields: [{
+        name: "email",
+        title: "Email",
+        type: "email"
+    }, {
+        name: "image",
+        title: "Image",
+        type: "url"
+    }, {
+        name: "name",
+        title: "Name",
+        type: "string"
+    }, {
+        name: "uid",
+        title: "UID",
+        type: "string"
+    }, 
+    { name: 'lastlogin', type: 'datetime', title: 'Last login' },]
+}
